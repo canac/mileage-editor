01e2d33ab07abfd70d1262259f5210cb82328734
Caleb Cox <canac@users.noreply.github.com>
2021-02-27 09:41:17 -0600

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  ignorePatterns: ['dist/'],
+  extends: [
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  overrides: [{
+    files: ['.eslintrc.js'],
+    parserOptions: {
+      sourceType: 'script',
+    },
+  }, {
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:vue/recommended',
+    ],
+    files: ['vite.config.ts', 'src/**/*.ts', 'src/**/*.vue'],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+      parser: '@typescript-eslint/parser',
+      project: 'tsconfig.json',
+      extraFileExtensions: ['.vue'],
+    },
+    rules: {
+      'import/extensions': 0,
+      'import/no-unresolved': 0,
+      'import/order': ['error', { alphabetize: { order: 'asc' } }],
+      'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    },
+  }],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', { code: 120 }],
+    strict: ['error', 'global'],
+  },
+};
