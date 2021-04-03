e26adc5020e63db522908ea04bb258d44cd25633
Caleb Cox <canac@users.noreply.github.com>
2021-04-03 08:50:39 -0500

    Initiate a preload connection to the GraphQL API

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import {
+  ConfigEnv, UserConfigExport, defineConfig, loadEnv,
+} from 'vite';
+import { injectHtml } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  // Load the environment variables manually because Vite doesn't load them automatically
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      injectHtml({
+        injectData: {
+          graphqlApiOrigin: new URL(env.VITE_GRAPHQL_ENDPOINT).origin,
+        },
+      }),
+    ],
+  });
+};
